3f7aa025ecf21cc7c07a1907d09a95d8091788d0
Tim <tim.terf@gmail.com>
2021-02-18 12:07:11 -0500

    released to the real pypi

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# docker build -t pennsive/whitestripe .
-
+docker build -t pennsive/whitestripe .
+rm -rf dist
 docker run --rm -it -v $PWD:/workspace -w /workspace pennsive/whitestripe python3 -m build
-docker run --rm -it -v $PWD:/workspace -w /workspace pennsive/whitestripe python3 -m twine upload --repository testpypi dist/*
+docker run --rm -it -v $PWD:/workspace -w /workspace pennsive/whitestripe python3 -m twine upload dist/*
